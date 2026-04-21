3ea0607d8c13fc21a30d4d87fa822a4566b19a11
Praveen Kumar Singh <prveensingh.dmr@gmail.com>
2026-04-21 23:58:47 +0530

    add Listing model with schema and geolocation fields

---
+++ models/listing.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import Review from "./review.js";
+import Joi from "joi";
+
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: String,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2h8ZW58MHx8MHx8fDA%3D",
+            set: (v) => 
+            v === ""
+            ? "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2h8ZW58MHx8MHx8fDA%3D" 
+            : v,
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
+    },
+    // category: {
+    //     type: String,
+    //     enum: ["montains", "arctic", "farms", "deserts"],
+    // },
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+export default Listing;
